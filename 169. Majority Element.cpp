a76c1c10cec476abc620dce0825830458a414987
Hojin00 <robert.ryu01@gmail.com>
2022-07-16 22:50:33 -0300

    169-new approach with differnt .find and .insert

--- 169. Majority Element.cpp
+++ 169. Majority Element.cpp
@@ -13,6 +13,9 @@ Output: 2
 
 **/
 
+// Runtime: 19 ms, faster than 90.99% of C++ online submissions for Majority Element.
+// Memory Usage: 19.5 MB, less than 58.28% of C++ online submissions for Majority Element.
+
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
@@ -32,3 +35,52 @@ public:
         return num;
     }
 };
+
+
+// Runtime: 29 ms, faster than 55.37% of C++ online submissions for Majority Element.
+// Memory Usage: 19.6 MB, less than 58.28% of C++ online submissions for Majority Element.
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        map<int, int> m;
+        
+        for(int i = 0; i < nums.size(); i++) {
+            int a = nums[i];
+            
+            // m.find(a) -> this line of code delays the runtime
+            if(m.find(a) == m.end()) {
+                auto p = m.find(a);
+                
+                m.insert(make_pair(a,1)); // and this line too.
+            } else {
+                auto p = m.find(a);
+                p->second = p->second + 1;
+                
+            }
+        }
+        
+        int max = nums.size()/2;
+        int result = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            int a = nums[i];
+            auto p = m.find(a);
+            
+            if(p->second > max) {
+                result = p->first;
+                break;
+            }
+        } 
+        return result;
+    }
+};
+
+//this way of insertion gives Runtime: 20 ms, faster than 87.84% of C++ online submissions for Majority Element. and same memory usage.
+for(int e : nums) {
+    if(m.find(e) == m.end()) {
+        m[e] = 1; // it's better update a new value or old value this way.
+    } else {
+        m[e] += 1;
+   	}
+            
+}
